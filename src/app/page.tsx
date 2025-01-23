5c1bd38dcdb9248ba966bb2e55915857bcb63199
muhammadfiras <muhfiras1@gmail.com>
2025-01-23 20:47:01 +0700

    fix X is unused

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Music, Instagram, Mail, X} from "lucide-react";
+import { Music, Instagram, Mail} from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
